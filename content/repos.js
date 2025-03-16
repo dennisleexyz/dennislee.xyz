058004c1320c8fb401b7cee687aade7418a2b47a
Dennis Lee <dennis@dennislee.xyz>
2025-03-15 21:21:59 -0700

    Support multiple repo owners

--- content/repos.js
+++ content/repos.js
@@ -1,4 +1,5 @@
 import { html } from "https://esm.sh/@arrow-js/core"
+const owners = ["dennisleexyz", "moduloindustries"]
 html`
   <thead>
     <tr>
@@ -6,15 +7,18 @@ html`
     </tr>
   </thead>
   <tbody>
-    ${await fetch("https://api.github.com/users/dennisleexyz/repos")
-      .then((response) => response.json())
+    ${await Promise.all(owners.map((owner) => fetch(`https://api.github.com/users/${owner}/repos`)))
+      .then((responses) => Promise.all(responses.map((response) => response.json())))
       .then((data) =>
         data
+          .flat()
           .sort((a, b) => b.size - a.size)
           .map(
             (repo) => `<tr>
       <td><a href=${repo.html_url}>${repo.name}</a></td>
-      <td>${repo.description + (repo.homepage ? ` <a href=${repo.homepage}>${repo.homepage}</a>` : "")}</td>
+      <td>${
+        repo.description + (repo.homepage ? ` <a href=${repo.homepage}>${repo.homepage}</a>` : "")
+      }</td>
       <td>${repo.language}</td>
     </tr>`,
           ),
