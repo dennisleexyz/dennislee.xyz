c392917fa1b826799465e789ba7618c929b6b472
Dennis Lee <dennis@dennislee.xyz>
2025-04-24 13:11:34 -0700

    Add badge/button row and valid RSS badge to Footer

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,13 +1,14 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
 import { version } from "../../package.json"
+import { GlobalConfiguration } from "../cfg"
 import { i18n } from "../i18n"
 
 interface Options {
   links: Record<string, string>
 }
 
-export default ((opts?: Options) => {
+export default ((opts?: Options, cfg: GlobalConfiguration) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
@@ -20,6 +21,11 @@ export default ((opts?: Options) => {
               {idx < arr.length - 1 && " | "}
             </>
           ))}
+        </div>
+        <div>
+          <a href={`http://validator.w3.org/feed/check.cgi?url=http%3A//${cfg.baseUrl}/${opts?.rssSlug ?? "index"}.xml`}>
+            <img src="valid-rss-rogers.png" alt="[Valid RSS]" title="Validate my RSS feed" />
+          </a>
         </div>
         {i18n(cfg.locale).components.footer.createdWith}{" "}
         <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
